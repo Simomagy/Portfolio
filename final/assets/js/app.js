3f6cff7821efd3387c6c4a507548e520f2752a83
Simone Magenes <83650442+Simomagy@users.noreply.github.com>
2023-11-06 23:03:58 +0100

    builded base homepage

--- final/assets/js/app.js
+++ final/assets/js/app.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
 
 function loadData()
 {
-	
-
+	let menu = $('.menu > *');
+	setTimeout(function() {
+		menu.css('opacity', 0);
+	1000});
+	anime({
+		targets: '.menu *',
+		scale: [14,1],
+		opacity: [0,1],
+		easing: "easeOutCirc",
+		duration: 400,
+		delay: anime.stagger(100, {easing: "easeOutCirc"})
+	});
 }
